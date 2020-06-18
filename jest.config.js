084195463f901dab1c40cfa0d9196eab928780d4
Deivid Rodrigues <deivid.arodrigues@gmail.com>
2020-06-18 12:49:31 -0300

    chore: add jest and your config

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  preset: 'ts-jest',
+}
--- src/middlewares/middleware.ts
+++
@@ -1,7 +0,0 @@
-const b = 30
-
-const c = (n: number): number => {
-  return 1
-}
-
-console.log(c(b))
