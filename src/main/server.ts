f65f6c00a4d6af88a8a845f4c10a5987b2a96168
Deivid Rodrigues <deivid.arodrigues@gmail.com>
2020-07-14 19:01:20 -0300

    feat: connect database from the server

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,12 @@
 import app from './config/app'
+import { createConnection } from 'typeorm'
 
-app.listen(3000, () => {
-  console.log('Server is running in http://localhost:3000...')
-})
+createConnection()
+  .then((connection) => {
+    app.listen(3000, () => {
+      console.log('Server is running in http://localhost:3000...')
+    })
+  })
+  .catch((error) => {
+    console.log('Connection to database was failed with error: ', error)
+  })
