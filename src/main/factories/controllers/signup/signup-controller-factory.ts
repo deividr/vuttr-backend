39c425365041fda9833cac6023fc500849f91205
Deivid Rodrigues <deivid.arodrigues@gmail.com>
2020-07-15 12:19:35 -0300

    feat: should LogDecorator envolved SignupController correct

--- src/main/factories/controllers/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -1,11 +1,14 @@
 import SignUpController from '../../../../api/controller/signup-controller'
+import Controller from '../../../../api/protocols/controller'
 import { DbCreateUser } from '../../../../data/usercases/create-user/db-create-user'
 import { BcryptAdapter } from '../../../../infra/criptography/bcrypt/bcrypt-adapter'
 import { UserRepository } from '../../../../infra/database/typeorm/repositories/user/user-repository'
+import LogDecorator from '../../../decorators/log-decorator'
 
-export default (): SignUpController => {
+export default (): Controller => {
   const encrypter = new BcryptAdapter()
   const createUserRepository = new UserRepository()
   const dbCreateUser = new DbCreateUser(encrypter, createUserRepository)
-  return new SignUpController(dbCreateUser)
+  const signupController = new SignUpController(dbCreateUser)
+  return new LogDecorator(signupController)
 }
