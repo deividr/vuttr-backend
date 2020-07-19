e33e4a7752b51adb8a4fd00ceb52096d786dc4f3
Deivid Rodrigues <deivid.arodrigues@gmail.com>
2020-07-19 10:28:39 -0300

    refactor: use faker lib to mock CreateUserParams

--- src/infra/database/typeorm/repositories/user/user-repository.spec.ts
+++ src/infra/database/typeorm/repositories/user/user-repository.spec.ts
@@ -1,6 +1,7 @@
 import { UserRepository } from './user-repository'
 import { createConnection, getConnection, getRepository } from 'typeorm'
 import { User } from '../../entities/User'
+import faker from 'faker'
 
 describe('User Repository Data Base', () => {
   beforeAll(async () => {
@@ -15,18 +16,18 @@ describe('User Repository Data Base', () => {
     const userRepository = new UserRepository()
 
     const createUserParams = {
-      name: 'valid_name',
-      email: 'valid_value@email.com.br',
-      password: 'valid_password',
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
     }
 
     const user = await userRepository.create(createUserParams)
 
     expect(user).toBeTruthy()
     expect(user.id).toBeTruthy()
-    expect(user.name).toBe('valid_name')
-    expect(user.email).toBe('valid_value@email.com.br')
-    expect(user.password).toBe('valid_password')
+    expect(user.name).toBe(createUserParams.name)
+    expect(user.email).toBe(createUserParams.email)
+    expect(user.password).toBe(createUserParams.password)
 
     await getRepository(User).delete(user.id)
   })
