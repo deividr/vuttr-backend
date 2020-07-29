a276e52aa2c33c7f8eae7d3fbf770fdce7ddb179
Deivid Rodrigues <deivid.arodrigues@gmail.com>
2020-07-29 16:54:18 -0300

    refactor: improve code on BcryptAdapter

--- src/infra/criptography/bcrypt/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt/bcrypt-adapter.spec.ts
@@ -5,35 +5,25 @@ describe('Bcrypt Adapter', () => {
   test('should call hash method with correct values', async () => {
     const sut = new BcryptAdapter()
     const spyHash = jest.spyOn(bcrypt, 'hash')
-
     await sut.hash('any_values')
-
     expect(spyHash).toHaveBeenCalledWith('any_values', 12)
   })
 
   test('should return throw if BcryptAdapter throw', async () => {
     const sut = new BcryptAdapter()
-
     jest
       .spyOn(bcrypt, 'hash')
       .mockResolvedValueOnce(Promise.reject(new Error()))
-
     const promise = sut.hash('valid_password')
-
     await expect(promise).rejects.toThrow()
   })
 
-  test('should encrypter return hashed value', async () => {
+  test('should hash method return hashed value', async () => {
     const sut = new BcryptAdapter()
-
-    const mock = jest.spyOn(bcrypt, 'hash').mockImplementation(async () => {
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(async () => {
       return 'hashed_values'
     })
-
     const hashed = await sut.hash('valid_password')
-
     expect(hashed).toEqual('hashed_values')
-
-    mock.mockRestore()
   })
 })
