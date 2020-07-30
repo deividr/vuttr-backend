8cab6da795db3a2addbd1f32bacf70ef69c3f6c1
Deivid Rodrigues <deivid.arodrigues@gmail.com>
2020-07-30 13:01:15 -0300

    refactor: create separated factory files

--- src/main/factories/controllers/login/login-controller-factory.ts
+++ src/main/factories/controllers/login/login-controller-factory.ts
@@ -1,24 +1,11 @@
 import { LoginController } from '../../../../presentation/controller/login/login-controller'
 import { Controller } from '../../../../presentation/protocols/controller'
-import { LogDecorator } from '../../../decorators/log-decorator'
-import { LogRepository } from '../../../../infra/database/typeorm/repositories/log/log-repository'
 import { LoginBodyRequestValidation } from '../../../../validation/validators/login/login-body-request-validation'
-import { DbAuthentication } from '../../../../data/usercases/user/authentication/db-authentication'
-import { UserRepository } from '../../../../infra/database/typeorm/repositories/user/user-repository'
-import { BcryptAdapter } from '../../../../infra/criptography/bcrypt/bcrypt-adapter'
-import { JwtAdapter } from '../../../../infra/criptography/jwt/jwt-adapter'
+import { makeDbAuthentication } from '../../usercases/user/authentication/db-authentication-factory'
+import { makeLogControllerDecorator } from '../../decorators/log-decorator-factory'
 
 export default (): Controller => {
-  const loadUserByEmailRepository = new UserRepository()
-  const bcryptAdapter = new BcryptAdapter()
-  const jwtAdapter = new JwtAdapter(process.env.JWT_SECRET as string)
-  const dbAuthentication = new DbAuthentication(
-    loadUserByEmailRepository,
-    bcryptAdapter,
-    jwtAdapter,
-  )
   const validation = new LoginBodyRequestValidation()
-  const loginController = new LoginController(validation, dbAuthentication)
-  const logErrorRepository = new LogRepository()
-  return new LogDecorator(loginController, logErrorRepository)
+  const controller = new LoginController(validation, makeDbAuthentication())
+  return makeLogControllerDecorator(controller)
 }
