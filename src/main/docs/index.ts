af7473d2851fa16035f77ba09b5389ce639bcdbf
Deivid Rodrigues <deivid.arodrigues@gmail.com>
2020-08-06 21:20:01 -0300

    feat: format header swagger

---
+++ src/main/docs/index.ts
@@ -0,0 +1,14 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'VUTTR - Very Useful Tools to Remember',
+    description:
+      'API to manage tools with their respective names, links, descriptions and tags',
+    version: '0.1.9',
+  },
+  servers: [
+    {
+      url: '/api',
+    },
+  ],
+}
