6623843259d439c11f4c7b86ba63afbde40bcdc7
Deivid Rodrigues <deivid.arodrigues@gmail.com>
2020-08-29 19:41:27 -0300

    refactor: move signup validation to login folder

--- src/main/factories/controllers/login/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/login/signup/signup-controller-factory.ts
@@ -1,6 +1,6 @@
 import { SignUpController } from '$/presentation/controller/login/signup/signup-controller'
 import { Controller } from '$/presentation/protocols/controller'
-import { SignupBodyRequestValidation } from '$/validation/validators/signup/body-request-validation'
+import { SignupBodyRequestValidation } from '$/validation/validators/login/signup/body-request-validation'
 import { makeLogControllerDecorator } from '$/main/factories/decorators/log-decorator-factory'
 import { makeDbAuthentication } from '$/main/factories/usercases/user/authentication/db-authentication-factory'
 import { makeDbCreateUser } from '$/main/factories/usercases/user/create-user/db-create-user-factory'
